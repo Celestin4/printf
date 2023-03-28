9be92ab8ad198c6804e7d3a5895a4608016c03e0
NTEZIRYAYO Celestin <97307891+Celestin4@users.noreply.github.com>
2023-03-28 16:54:37 +0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,146 +1,188 @@
 #include "main.h"
 
-/**
- * print_ch - print single char at a time
- * @args: list of runtime arguments
- * @count: pointer to count of printed bytes
- * @sz: size specifier
- *
- * Return: 0
- */
-int print_ch(va_list args, int *count, int sz)
-{
-	int x;
+/************************* PRINT CHAR *************************/
 
-	UN_NEEDED(sz);
-	x = va_arg(args, int);
-	write(1, &x, 1);
-	(*count)++;
-	return (0);
+/**
+ * print_char - Prints a char
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	char c = va_arg(types, int);
+
+	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
+/************************* PRINT A STRING *************************/
 /**
- * print_str - print a string parameter
- * @pars: the list of variable type arguments
- * @count: pointer to number of chars printed so far
- * @sz: size specifier(unneeded here)
- *
- * Return: 0
+ * print_string - Prints a string
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
  */
-int print_str(va_list pars, int *count, int sz)
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
 {
-	char *x;
+	int length = 0, i;
+	char *str = va_arg(types, char *);
 
-	UN_NEEDED(sz);
-	x = va_arg(pars, char *);
-	if (x == NULL)
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	if (str == NULL)
 	{
-		char *nil = "(null)";
-
-		write(1, nil, 6);
-		*count += 6;
+		str = "(null)";
+		if (precision >= 6)
+			str = "      ";
 	}
-	else
+
+	while (str[length] != '\0')
+		length++;
+
+	if (precision >= 0 && precision < length)
+		length = precision;
+
+	if (width > length)
 	{
-		while (*x != '\0')
+		if (flags & F_MINUS)
 		{
-			_putchar(*x);
-			(*count)++;
-			x++;
+			write(1, &str[0], length);
+			for (i = width - length; i > 0; i--)
+				write(1, " ", 1);
+			return (width);
+		}
+		else
+		{
+			for (i = width - length; i > 0; i--)
+				write(1, " ", 1);
+			write(1, &str[0], length);
+			return (width);
 		}
 	}
-	return (0);
+
+	return (write(1, str, length));
+}
+/************************* PRINT PERCENT SIGN *************************/
+/**
+ * print_percent - Prints a percent sign
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int print_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	UNUSED(types);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	return (write(1, "%%", 1));
 }
 
+/************************* PRINT INT *************************/
 /**
- * binconv - convert to binary
- * @args: list of runtime arguments
- * @count: pointer to number of bytes computed in the
- *	calling function
- * @sz: size specifier, unneeded here
- *
- *Return: 0
+ * print_int - Print int
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
  */
-int binconv(va_list args, int *count, int sz)
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
 {
-	int i, x;
-	char bits[100];
+	int i = BUFF_SIZE - 2;
+	int is_negative = 0;
+	long int n = va_arg(types, long int);
+	unsigned long int num;
 
-	UN_NEEDED(sz);
-	i = 0;
-	x = va_arg(args, int);
-	while (x > 0)
-	{
-		bits[i++] = x % 2 + '0';
-		x /= 2;
-	}
-	for (i -= 1; i >= 0; i--)
-	{
-		printer(bits, i);
-		(*count)++;
-	}
-	return (0);
-}
-/**
- * print_percent - print a percent sign
- * @list: a list of runtime arguments
- * @count: pointer to number of chars printed
- * @sz: the size specifier
- *
- * Return: 0
- */
-int print_percent(va_list list, int *count, int sz)
-{
-	char x;
+	n = convert_size_number(n, size);
 
-	UN_NEEDED(sz);
-	UN_NEEDED(list);
-	x = '%';
-	_putchar(x);
-	(*count)++;
-	return (0);
+	if (n == 0)
+		buffer[i--] = '0';
+
+	buffer[BUFF_SIZE - 1] = '\0';
+	num = (unsigned long int)n;
+
+	if (n < 0)
+	{
+		num = (unsigned long int)((-1) * n);
+		is_negative = 1;
+	}
+
+	while (num > 0)
+	{
+		buffer[i--] = (num % 10) + '0';
+		num /= 10;
+	}
+
+	i++;
+
+	return (write_number(is_negative, i, buffer, flags, width, precision, size));
 }
 
+/************************* PRINT BINARY *************************/
 /**
- * print_mem - print memory address
- * @list: list of runtime arguments
- * @count: address of memory containing number of
- * printed bytes so far
- * @sz: the size specifier
- *
- * Return: 0
+ * print_binary - Prints an unsigned number
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Numbers of char printed.
  */
-int print_mem(va_list list, int *count, int sz)
+int print_binary(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
 {
-	char address[BUFF];
-	unsigned long int x, mod, i;
-	char hex[] = "0123456789abcdef";
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int count;
 
-	UN_NEEDED(sz);
-	x = va_arg(list, unsigned long int);
-	if (!x)
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	n = va_arg(types, unsigned int);
+	m = 2147483648; /* (2 ^ 31) */
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
 	{
-		char *null = "(nil)";
-
-		write(1, null, 5);
-		*count += 5;
+		m /= 2;
+		a[i] = (n / m) % 2;
 	}
-	else
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
 	{
-		_putchar('0');
-		_putchar('x');
-		*count += 2;
-		for (i = 0; x > 0; x /= 16)
+		sum += a[i];
+		if (sum || i == 31)
 		{
-			mod = x % 16;
-			address[i++] = hex[mod];
-		}
-		for (i -= 1;; i--)
-		{
-			_putchar(address[i]);
-			(*count)++;
-			if (address + i == address)
-				break;
+			char z = '0' + a[i];
+
+			write(1, &z, 1);
+			count++;
 		}
 	}
-	return (0);
+	return (count);
 }
